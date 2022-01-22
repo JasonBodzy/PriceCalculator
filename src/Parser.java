a15c3c9c98d83efb382a9f7be603426efaa7e20b
jasonbodzy <jasonbodzy@gmail.com>
2022-01-22 17:56:55 -0500

    initial

---
+++ src/Parser.java
@@ -0,0 +1,11 @@
+public class Parser {
+    String closePrices;
+    public Parser(String closePrices) {
+        this.closePrices = closePrices; //txt file with close prices
+    }
+
+    double[] getClosePrices() { // parses and makes an array of close prices
+        double[] closePricesArray = {};
+        return closePricesArray;
+    }
+}
