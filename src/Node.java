8b95fab3aad393eb5921d33e59f4e859c7f70788
jasonbodzy <jasonbodzy@gmail.com>
2022-01-22 18:38:11 -0500

    fitness tests

--- src/Node.java
+++ src/Node.java
@@ -3,7 +3,11 @@ public class Node {
     private double[] closePrices;
     private double hiddenPred;
     private Node node;
+    private double fitness;
 
+    public Node() {
+
+    }
     public Node(double[] closePrices) {
         this.closePrices = closePrices;
         this.hiddenPred = Math.random();
@@ -16,10 +20,18 @@ public class Node {
     }
 
     public double getHiddenPred() {
-        return hiddenPred;
+        return this.hiddenPred;
     }
 
-    boolean[] netCalculate() {
+    public double getFitness() {
+        return this.fitness;
+    }
+
+    public void setFitness(double fitness) {
+        this.fitness = fitness;
+    }
+
+    public boolean[] netCalculate() {
         boolean[] calculations = new boolean[closePrices.length];
 
         for (int i = 0;  i < closePrices.length; i++) {
@@ -33,14 +45,19 @@ public class Node {
         return calculations;
     }
 
-    boolean calculate(double[] pastPrices) {
+    private boolean calculate(double[] pastPrices) {
         double prediction = 0;
         for (int i = 0; i < pastPrices.length; i ++) {
             double weight = ((double)i / (double)pastPrices.length);
-
             prediction += weight * pastPrices[i] * hiddenPred;
-
         }
+
+        System.out.println("Prediction: " + prediction);
+
+        if (prediction <= 1) {
+            return true;
+        }
+        return false;
 
     }
 }
--- src/NodeManager.java
+++ src/NodeManager.java
@@ -1,3 +1,41 @@
 public class NodeManager {
-    private Node[] nodes;
+    private Node[] nodes = new Node[100];
+    private double[] closingPrices;
+
+    public NodeManager(double[] closingPrices) {
+        this.closingPrices = closingPrices;
+    }
+
+    public Node runNodes() {
+        Node baseNode = new Node(this.closingPrices);
+        nodes[0] = baseNode;
+        for (int i = 1; i < nodes.length; i++) {
+            nodes[i] = new Node(this.closingPrices, nodes[i - 1]);
+        }
+
+        return(findBest(nodes));
+    }
+
+    public Node findBest(Node[] nodes) {
+        double fitness = -1;
+        Node topNode = new Node();
+        for (int i = 0; i < nodes.length; i++) {
+            double thisFitness = 0;
+            for (int c = 0; c < closingPrices.length; c++) {
+                if (c != closingPrices.length - 1) {
+                    if ((nodes[i].netCalculate()[c]) && closingPrices[i + 1] - closingPrices[i] > 0) {
+                        thisFitness++;
+                    }
+                }
+            }
+            if (thisFitness > fitness) {
+                fitness = thisFitness;
+                topNode = nodes[i];
+            }
+        }
+
+        topNode.setFitness(fitness / closingPrices.length);
+        return(topNode);
+    }
+
 }
